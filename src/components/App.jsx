6fad5dfda8a16975cb86a47ab739e79ab5ce9ad2
Marina Bibichenko <108667532+MarinaUser@users.noreply.github.com>
2023-02-06 13:28:54 +0200

    save contacts to localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,10 @@ import { ContactList } from './ContactList/ContactList';
 
 import { Title, Contacts } from './App.styled';
 
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     
   };
@@ -58,6 +54,24 @@ export class App extends Component {
   // formSubmitHandler = data => {
   //   console.log(data);
   // }
+
+  componentDidMount() {
+
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts })
+    } 
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+   
+    if (this.state.contacts !== prevState.contacts) {
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
 
   render() {
     const visibleContacts = this.getVisibleContacts();
